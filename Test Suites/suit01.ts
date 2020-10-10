a190e787af3aa348fdf596c4d3e7a94ccaf9f55f
Duc <paypal.dhtran789@gmail.com>
2020-10-10 16:49:15 +0700

    Lap4

---
+++ Test Suites/suit01.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>suit01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f9878130-e194-4ffb-9e8f-4ebd841de289</testSuiteGuid>
+   <testCaseLink>
+      <guid>6c398dad-4511-4ec4-8837-0e95e118a2af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc02_profile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
